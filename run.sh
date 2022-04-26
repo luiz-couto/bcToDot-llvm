7a99626de6ae338bd3b093d7207f44b0d8b01eb5
luiz-couto <luiz.felipe.556@outlook.com>
2022-04-26 19:01:40 -0300

    organized benchmarks

--- run.sh
+++ run.sh
@@ -7,8 +7,14 @@ for d in */ ; do
     echo $bench
 
     cd $bench
+    
+    mkdir src
+    mv ${bench}.c src/${bench}.c
+
     mkdir IR
-    mv ${bench}.ll src/${bench}.ll
+    mv ${bench}.ll IR/${bench}.ll
+    
+
     cd ..
 
 done
